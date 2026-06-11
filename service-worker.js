af8304b9198988e05004289416fa3b742ce0b710
tpemmosquito <tommy.pemrick.clarke@gmail.com>
2026-06-11 10:08:01 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "bioassay-data-entry-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
